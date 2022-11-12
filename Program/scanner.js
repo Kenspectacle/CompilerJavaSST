f0494e43643a84838a3d3dc25c2a4a30189acd2d
Kenny <kennyftw2@gmail.com>
2022-11-12 20:50:29 +0100

    first commit

---
+++ Program/scanner.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+const fileContents = fs.readFileSync('./myFile').toString()
+
+class Scanner {
+    constructor(input, char, )
+}
+
+
+
+
+
+
+
+
+
+
+
+let text = ""
+let start = ""
+
+
+
+let next = () => {
+    return;
+}
+
+let error = () => {
+    console.error("Invalid First Character")
+}
+
+let first = (ch) => {
+    //check what kind of character is it
+    switch(c) {
+        case 'i':
+            break; //possibility of if
+        default:
+            error();
+    }
+}
+
+let scanner = (text) => {
+
+}
