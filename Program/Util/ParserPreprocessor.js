b01a3cb1caa2a220fa3f57322a2c1f30f4f85c3e
Kenny <kennyftw2@gmail.com>
2022-11-27 23:12:57 +0100

    implemented preprocessing for /* */ comments, removing everything in between

--- Program/Util/ParserPreprocessor.js
+++ Program/Util/ParserPreprocessor.js
@@ -16,14 +16,13 @@ console.log(scannedText);
 let parserPreprocessor = (scannedText) => {
     //Removing Comments
 
-    //Comments with //
+    //Comments using two pointers
     let p1 = 0 //search index pointer 1
     let p2 = 0 //search index pointer 2
     while(p1 != text.length - 1) {
-        
+        //remove comments with "//"
         if(scannedText[p1] === "//") {
-            p2 = p1 + 1
-            console.log(p1);
+            p2 = p1 + 1;
             if(scannedText[p2] === "\n" || scannedText[p2] === undefined) {
                 scannedText.splice(p1, 1); //removes the "//"
                 continue;
@@ -31,9 +30,30 @@ let parserPreprocessor = (scannedText) => {
             scannedText.splice(p2, 1); //remove one element next to it
             continue;
         }
+        //remove comments with /* */ terminals
+        if(scannedText[p1] === "/*") {
+            //intiialize
+            if(p1 > p2) {
+                p2 = p1 + 1;
+            }
+            //skip nextlines, important for debugging
+            if(scannedText[p2] === "\n") {
+                p2++;
+                continue;
+            }
+            //base case p2 is the same as "*/"
+            if(scannedText[p2] === "*/") {
+                scannedText.splice(p2, 1); //remove ending */
+                scannedText.splice(p1, 1); //remove starting /*
+                continue;
+            }
+            //remove the rest
+            scannedText.splice(p2, 1); //remove element at p2
+            continue;
+        }
         p1++;
+        
     }
-    console.log(scannedText);
     return scannedText;
 };
 
