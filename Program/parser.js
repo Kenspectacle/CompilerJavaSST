06a99123802bcf390b08e58768340e7a796ac78e
Kenny <kennyftw2@gmail.com>
2022-11-27 19:34:30 +0100

    connected symboltable.js with parser.js

--- Program/parser.js
+++ Program/parser.js
@@ -8,7 +8,12 @@ Will be using the symbol table to check for keywords
 const fs = require('fs') //read file
 const path = require('path');
 const Scanner = require(path.resolve(__dirname, "./scannerv2.js")); // scanner
+const symbolTable = require(path.resolve(__dirname, "./symboltable.js")); //symbol table
 
 let text = fs.readFileSync('./input.txt').toString()
+
+let Parser = (text, symboltable) => {
+    
+}
 
 console.log(Scanner(text));
