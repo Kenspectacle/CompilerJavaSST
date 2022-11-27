88880551f341c18f1f66d1aa86071e8ca0596bd6
Kenny <kennyftw2@gmail.com>
2022-11-27 18:30:32 +0100

    implemented basic scanner, need edge case(identifier with special symbol in the middle) to be implemented

--- Program/scannerv2.js
+++ Program/scannerv2.js
@@ -4,9 +4,35 @@ Datastructure used: Array
 Type of export: Scanner Function
 */
 
-let Scanner = (text, symbols) => {
-    return 0;
+let Scanner = (text) => {
+    //initialize
+    n = 0 //index search n
+    str = "" //current string
+
+    //keywords and identifiers within text
+    let result = []; 
+
+    //base cases, ignore whitespace and newline at the end
+    text.trim();
+    console.log('testsss')
+    console.log(text);
+    while(text.length > 0) {
+        //base cases ignore whitespace and newline
+        if(text[0] === " " || text[0] === "\n") {
+            text = text.slice(1);
+        }
+        n = 0
+        str = ""
+        console.log('qwewqeqe')
+        //look for words
+        while(text[n] !== " " && text[n] !== "\n" && text[n] !== undefined) {
+            str += text[n]
+            n++;
+        }
+        result.push(str);
+        text = text.slice(n);
+    }
+    return result
 }
 
-console.log(Scanner(1,2));
-
+module.exports.Scanner;
