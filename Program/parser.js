f0bfc964936d9a6510c5c970407a200878e5cf51
Kenny <kennyftw2@gmail.com>
2022-11-27 18:32:20 +0100

    added file reader to parser

--- Program/parser.js
+++ Program/parser.js
@@ -4,3 +4,7 @@ Used to parse through the output of the scanner, see if the syntax are valid.
 Will be using the symbol table to check for keywords
 
 */
+
+const fs = require('fs') //read file
+
+let text = fs.readFileSync('./input.txt').toString()
