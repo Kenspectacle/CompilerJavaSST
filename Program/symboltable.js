aec8d67526b499ec4a69569aa1990084b8b36eef
Kenny <kennyftw2@gmail.com>
2022-11-27 19:56:43 +0100

    filled symbol table and added export

--- Program/symboltable.js
+++ Program/symboltable.js
@@ -3,4 +3,41 @@ Symbol table for JavaSST
 Here, symbols are defined and exported in a form of an Array
 */
 
-let Symbols = [];
+let Symbols = [
+    '(', 
+    ')', 
+    '+', 
+    '*', 
+    '{',
+    '}', 
+    '[', 
+    ']',
+    "<",
+    ">",
+    "<=",
+    ">=",
+    "=",
+    "+",
+    "-",
+    "*",
+    "/",
+    ".",
+    ";",
+    ","
+]; //basic symbols
+
+//keywords
+Symbol.push('if');
+Symbol.push('else');
+Symbol.push('class');
+Symbol.push('final');
+Symbol.push('void');
+Symbol.push('int');
+Symbol.push('while');
+Symbol.push('public');
+Symbol.push('return');
+Symbol.push('IDENTIFIER')
+Symbol.push('NUMBER')
+Symbol.push('OTHER')
+
+module.exports = Symbols;
