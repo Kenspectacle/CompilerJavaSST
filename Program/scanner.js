a4289dd1c692bc3530e9d9bc94bee10c21c9a770
Kenny <kennyftw2@gmail.com>
2023-01-23 12:52:10 +0100

    changed token array into array of objects

--- Program/scanner.js
+++ Program/scanner.js
@@ -1,15 +1,16 @@
 /*
 JavaSST Scanner
-Checks the token by scanning the characters one by one
+Checks the content by scanning the characters one by one
 outputs an array
 
 List of keywords in JavaSST:
 class, public, void, if, int, final, return, else
 
 To Do List
--Refactor the code
+-Refactor the code -> done
 -Implement ignore feature for comments
 -Add keywords entry for the special keyword
+-Fix column location to give it proper feedback
 */
 
 
@@ -20,21 +21,22 @@ let text = fs.readFileSync(path.resolve(__dirname, "./input.txt")).toString();
 
 console.log(text);
 
-//initialize variables
-let start = "";
-let identifier = "";//for each identifiers
-let identifierGroup = []; //group up identifer later on
-let token = []; //make a list of tokens
-let tokenType = []; //this describes, what kind of token it is
-let newToken = {};
-let locColumn = 0;
-let locLine = 0;
+
 
 //single symbol list
 let singleSymbols = ['(', ')', '+', '*', '{', '}', '[', ']', ';', '-', ',', '/'];
 
 //iterate through the text
 let scanner = (text) => {
+    //initialize variables
+    let start = "";
+    let identifier = "";//for each identifiers
+    let identifierGroup = []; //group up identifer later on
+    let content = []; //make a list of contents
+    let contentType = []; //this describes, what kind of content it is
+    let tokens = [];
+    let locColumn = 0;
+    let locLine = 0;
     while (text.length > 0) {
         identifier = ""; //reinitialize the identifier
         //find starting symbol
@@ -43,58 +45,62 @@ let scanner = (text) => {
         switch (true) {
             //basic cases, single symbols
             case singleSymbols.includes(start):
-                token.push(start);
-                tokenType.push(start);
+                locColumn++;
+                tokens.push(new Object({ content: start, contentType: start, locColumn: locColumn, locLine: locLine }));
                 text = text.slice(1); //take out current index and move to next one
                 break;
             //empty space
             case start === " ":
                 text = text.slice(1);
+                locColumn++;
                 break;
             //new lines
             case start === "\n":
                 text = text.slice(1);
+                locLine++;
+                locColumn = 0;
                 break;
             //tabs
             case start === "\t":
                 text = text.slice(1);
+                locColumn += 4; //assumption, 1 tab = 4 columns
                 break;
             //comparison or equal
-            case start === "<" || start === "=" || start ===">":
-                if(text[0] + text[1] === "<=") {
+            case start === "<" || start === "=" || start === ">":
+                if (text[0] + text[1] === "<=") {
                     //less than equal
-                    token.push('<=');
-                    tokenType.push('<=');
+                    locColumn += 2;
+                    tokens.push({ content: '<=', contentType: '<=', locColumn: locColumn, locLine: locLine });
                     text = text.slice(2);
                     break;
-                } else if(text[0] + text[1] === ">=") {
+                } else if (text[0] + text[1] === ">=") {
                     //more than equal
-                    token.push('>=');
-                    tokenType.push('>=');
+                    locColumn += 2;
+                    tokens.push({ content: '<=', contentType: '<=', locColumn: locColumn, locLine: locLine });
                     text = text.slice(2);
                     break;
-                } else if(text[0] + text[1] === "==") {
+                } else if (text[0] + text[1] === "==") {
                     //equal equal
-                    token.push('==');
-                    tokenType.push('==');
+                    locColumn += 2;
+                    tokens.push({ content: '==', contentType: '==', locColumn: locColumn, locLine: locLine });
                     text = text.slice(2);
                     break;
-                } else if(text[0] === "=" && (text[1] === undefined || text[1] === " ")) {
+                } else if (text[0] === "=" && (text[1] === undefined || text[1] === " ")) {
                     //single equal
-                    token.push('=');
-                    tokenType.push('=');
+                    locColumn++;
+                    tokens.push({ content: '=', contentType: '=', locColumn: locColumn, locLine: locLine });
                     text = text.slice(1);
                     break;
-                } else if(text[0] === "<" && (text[1] === undefined || text[1] === " ")) {
+                } else if (text[0] === "<" && (text[1] === undefined || text[1] === " ")) {
                     //less than
-                    token.push('<');
-                    tokenType.push('<');
+                    locColumn++;
+                    tokens.push({ content: '<', contentType: '<', locColumn: locColumn, locLine: locLine });
                     text = text.slice(1);
                     break;
-                } else if(text[0] === ">" && (text[1] === undefined || text[1] === " ")) {
+                } else if (text[0] === ">" && (text[1] === undefined || text[1] === " ")) {
                     //more than
-                    token.push('>');
-                    tokenType.push('>');
+                    locColumn++;
+                    tokens.push({ content: '>', contentType: '>', locColumn: locColumn, locLine: locLine });
                     text = text.slice(1);
                     break;
                 }
@@ -106,9 +112,9 @@ let scanner = (text) => {
                     identifier += text[n]
                     n += 1;
                 }
+                locColumn += n;
                 text = text.slice(n)
-                token.push(identifier);
-                tokenType.push('number');
+                tokens.push({ content: identifier, contentType: 'number', locColumn: locColumn, locLine: locLine });
                 break;
             //alphabet
             case /^[a-zA-z]/.test(start):
@@ -116,8 +122,8 @@ let scanner = (text) => {
                 // console.log('test');
                 if (text[1] === undefined || text[1] === " ") {
                     //end of string
-                    token.push(start);
-                    tokenType.push('identifier');
+                    locColumn++;
+                    tokens.push({ content: start, contentType: 'identifier', locColumn: locColumn, locLine: locLine });
                     text = text.slice(1);
                     break;
                 }
@@ -126,18 +132,18 @@ let scanner = (text) => {
                     case ('c'):
                         //class keyword
                         if ((text[1] + text[2] + text[3] + text[4]) === "lass" && (text[5] === undefined || text[5]) === " ") {
+                            locColumn += 5;
+                            tokens.push({ content: 'class', contentType: 'class', locColumn: locColumn, locLine: locLine });
                             text = text.slice(5);
-                            token.push('class');
-                            tokenType.push('class');
                             break;
                         }
                         continue;
                     case ('p'):
                         //public keyword
                         if ((text[1] + text[2] + text[3] + text[4] + text[5]) === "ublic" && text[6] === undefined || text[6] === " ") {
+                            locColumn += 6;
                             text = text.slice(6);
-                            token.push('public');
-                            tokenType.push('public');
+                            tokens.push({ content: 'public', contentType: 'public', locColumn: locColumn, locLine: locLine });
                             break;
                         }
                         continue;
@@ -145,52 +151,54 @@ let scanner = (text) => {
                         //void keyword
                         if ((text[1] + text[2] + text[3] === "oid") && (text[4] === undefined || text[4] === " ")) {
                             text = text.slice(4);
-                            token.push('void');
-                            tokenType.push('void');
+                            locColumn += 4;
+                            tokens.push({ content: 'void', contentType: 'void', locColumn: locColumn, locLine: locLine });
+                            content.push('void');
+                            contentType.push('void');
                             break;
                         }
                     case ('i'):
                         //if keyword
                         if (text[1] === "f" && (text[2] === undefined || text[2] === " " || text[2] === "(")) {
+                            locColumn += 2;
                             text = text.slice(2);
-                            token.push('if');
-                            tokenType.push('if');
+                            tokens.push({ content: 'if', contentType: 'if', locColumn: locColumn, locLine: locLine });
                             break;
                         } else if (text[1] + text[2] === "nt" && (text[3] === undefined || text[3] === " ")) {
                             //int keyword
-                            token.push('int');
-                            tokenType.push('int');
+                            locColumn += 3;
+                            tokens.push({ content: 'int', contentType: 'int', locColumn: locColumn, locLine: locLine });
                             text = text.slice(3);
                             break;
                         }
                     case ('f'):
                         //final keyword
                         if (text[1] + text[2] + text[3] + text[4] === "inal" && (text[5] === undefined || text[5] === " ")) {
-                            token.push('final');
-                            tokenType.push('final');
+                            locColumn += 5;
+                            tokens.push({ content: 'final', contentType: 'final', locColumn: locColumn, locLine: locLine });
                             text = text.slice(5);
                             break;
                         }
                     case ('r'):
                         if (text[1] + text[2] + text[3] + text[4] + text[5] === "eturn" && (text[6] === undefined || text[6] === " " || text[6] === ";")) {
-                            token.push('return');
-                            tokenType.push('return');
+                            locColumn += 6;
+                            tokens.push({ content: 'return', contentType: 'return', locColumn: locColumn, locLine: locLine });
                             text = text.slice(6);
                             break;
                         }
                     case ('e'):
                         //else keyword
                         if (text[1] + text[2] + text[3] === "lse" && (text[4] === undefined || text[4] === " ")) {
-                            token.push('else');
-                            tokenType.push('else');
+                            locColumn += 4;
+                            tokens.push({ content: 'else', contentType: 'else', locColumn: locColumn, locLine: locLine });
                             text = text.slice(4);
                             break;
                         }
                     case ('w'):
                         //while keyword
                         if (text[1] + text[2] + text[3] + text[4] === "hile" && (text[5] === undefined || text[5] === " " || text[5]) === "(") {
-                            token.push('while');
-                            tokenType.push('while');
+                            locColumn += 5;
+                            tokens.push({ content: 'while', contentType: 'while', locColumn: locColumn, locLine: locLine });
                             text = text.slice(5);
                             break;
                         }
@@ -201,17 +209,16 @@ let scanner = (text) => {
                             identifier += text[n]
                             n++;
                         }
+                        locColumn += n;
                         text = text.slice(n);
-                        token.push(identifier);
-                        tokenType.push("identifier");
+                        tokens.push({ content: identifier, contentType: 'identifier', locColumn: locColumn, locLine: locLine });
                         break;
                 }
         }
     }
-    return token, tokenType;
+    return tokens;
 }
 
-token, tokenType = scanner(text);
-console.dir(token, {'maxArrayLength': null});
-console.dir(tokenType, {'maxArrayLength': null});
-module.exports = {token, tokenType};
+tokens = scanner(text);
+console.dir(tokens, { 'maxArrayLength': null });
+module.exports = { tokens };
